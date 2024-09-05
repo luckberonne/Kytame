d6348a3cc611e7f7a28347b8a55f8d88e0dbe916
luckberonne <luckberonne@gmail.com>
2024-09-04 21:56:28 -0300

    Fix de errores con un poco de cabeza

--- Kytame/Hubs/ScoreboardHub.cs
+++ Kytame/Hubs/ScoreboardHub.cs
@@ -1,32 +1,10 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 public class ScoreboardHub : Hub
 {
     private static readonly Dictionary<string, (int team1Score, int team2Score, int team1Penalty, int team2Penalty, int timerMinutes, int timerSeconds, bool timerRunning, int currentRound)> GroupStates = new();
-    private static readonly Dictionary<string, HashSet<string>> GroupConnections = new();
-
-    public override Task OnConnectedAsync()
-    {
-        // No need to do anything special here, but it's good to override if needed
-        return base.OnConnectedAsync();
-    }
-
-    public override async Task OnDisconnectedAsync(System.Exception exception)
-    {
-        foreach (var group in GroupConnections.Where(g => g.Value.Contains(Context.ConnectionId)).ToList())
-        {
-            group.Value.Remove(Context.ConnectionId);
-            if (!group.Value.Any())
-            {
-                GroupConnections.Remove(group.Key);
-            }
-        }
-
-        await base.OnDisconnectedAsync(exception);
-    }
 
     public async Task JoinGroup(string groupName)
     {
@@ -37,12 +15,6 @@ public class ScoreboardHub : Hub
             GroupStates[groupName] = (0, 0, 0, 0, 1, 50, false, 1); // Initialize default values
         }
 
-        if (!GroupConnections.ContainsKey(groupName))
-        {
-            GroupConnections[groupName] = new HashSet<string>();
-        }
-        GroupConnections[groupName].Add(Context.ConnectionId);
-
         var state = GroupStates[groupName];
         await Clients.Caller.SendAsync("ReceiveScoreUpdate", state.team1Score, state.team2Score, state.team1Penalty, state.team2Penalty);
         await Clients.Caller.SendAsync("ReceiveTimerUpdate", state.timerMinutes, state.timerSeconds, state.timerRunning);
@@ -52,15 +24,6 @@ public class ScoreboardHub : Hub
     public async Task LeaveGroup(string groupName)
     {
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-
-        if (GroupConnections.ContainsKey(groupName))
-        {
-            GroupConnections[groupName].Remove(Context.ConnectionId);
-            if (!GroupConnections[groupName].Any())
-            {
-                GroupConnections.Remove(groupName);
-            }
-        }
     }
 
     public async Task UpdateScore(string groupName, int team1Score, int team2Score, int team1Penalty, int team2Penalty)
@@ -92,9 +55,9 @@ public class ScoreboardHub : Hub
 
     public Task<bool> AreClientsConnected(string groupName)
     {
-        if (GroupConnections.ContainsKey(groupName))
+        if (GroupStates.ContainsKey(groupName))
         {
-            return Task.FromResult(GroupConnections[groupName].Any());
+            return Task.FromResult(true);
         }
         return Task.FromResult(false);
     }
